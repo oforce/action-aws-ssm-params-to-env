52d326c79dd7b73a812380254e8c65c6713cc9c6
Chris Martin <chris.martin@oforce.com>
2020-03-06 15:56:35 -0700

    use params as input name

--- ssm.test.js
+++ ssm.test.js
@@ -8,3 +8,48 @@ it('should work', async () => {
   expect(values[0].variable).toEqual('OF_TEST');
   expect(values[0].value).not.toBeNull();
 });
+
+it('lksd', () => {
+  const vsl = [
+    {
+      path: '/prod/sqs/contractors_contracted',
+      variable: 'OF_SQS__CONTRACTORS_CONTRACTED'
+    },
+    {
+      path: '/prod/contractors/database/connection',
+      variable: 'OF_CONTRACTORS__DATABASE__CONNECTION'
+    },
+    {
+      path: '/prod/api/database/connection',
+      variable: 'OF_API__DATABASE__CONNECTION'
+    },
+    {
+      path: '/prod/icm/database/connection',
+      variable: 'OF_ICM__DATABASE__CONNECTION'
+    },
+    {
+      path: '/prod/settlement/pii_key',
+      variable: 'OF_SETTLEMENT__PII_KEY'
+    },
+    {
+      path: '/prod/api-management/uri',
+      variable: 'OF_API-MANAGEMENT__URI'
+    },
+    {
+      path: '/prod/oauth/client_id',
+      variable: 'OF_OAUTH__CLIENTID'
+    },
+    {
+      path: '/prod/oauth/client_secret',
+      variable: 'OF_OAUTH__CLIENTSECRET'
+    },
+    {
+      path: '/prod/oauth/token_uri',
+      variable: 'OF_OAUTH__TOKENURI'
+    },
+    {
+      path: '/prod/oauth/scope',
+      variable: 'OF_OAUTH__SCOPE'
+    }
+  ];
+});
