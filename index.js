272d883c3bf27f9ea7028ed6ad7131cf0c0463b8
Chris Martin <chris.martin@oforce.com>
2020-03-06 16:00:38 -0700

    dewbug

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { getParameters } = require('./ssm');
 
 async function run() {
   const params = core.getInput('params', { required: true });
+  console.log('Chris: run -> params', params);
+
   const values = await getParameters(JSON.parse(params));
   values.forEach(setEnvironmentVariable);
 }
